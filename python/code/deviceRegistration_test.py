2f61896d9754271a764a1d6ab4fd49d4eb41d0ca
adavidson32 <alex@davidson1.com>
2017-04-03 22:45:23 -0400

    New test copy of Device Registration w/ debug

---
+++ python/code/deviceRegistration_test.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+#-----------------------------------------------------------------------------------
+def getMAC(interface):
+  try:
+    str = open('/sys/class/net/' + interface + '/address').read()
+  except:
+    str = "00:00:00:00:00:00"
+  return str[0:17]
+
+def getserial():
+  # Extract serial from cpuinfo file
+  cpuserial = "0000000000000000"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+    f.close()
+  except:
+    cpuserial = "ERROR000000000"
+  return cpuserial
+#-----------------------------------------------------------------------------------
+
+device_serial = getserial()
+device_mac = getMAC('wlan0')
+print("Device Serial Number: " + device_serial)
+print("Device MAC Addr: " + device_mac)
+print("device_serial file type: " + type(device_serial))
+print("device_mac file type: " + type(device_mac))
+
+header = {'deviceIdentifier': device_serial, 'macAddress': device_mac}
+header_json = json.dumps(header)
+print("Header Type: " + type(header) + ". Header Text: " + header)
+print("Header_json Type: " + type(header_json) + ". Header_json Text: " + header_json)
+rep_json = requests.post("http://espressotg.info/eotg/api/devices/registerDevice", data=header_json)
+rep = rep_json.json()
+rep_ID = rep['deviceId']
+print("Assigned ID Number: " + str(rep_ID))
