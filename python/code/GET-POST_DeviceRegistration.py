b412c87db235a958130e6c4849f41f3cfecfe028
adavidson32 <alex@davidson1.com>
2017-04-03 20:34:53 -0400

    Updated getMAC input to include (' ')

--- python/code/GET-POST_DeviceRegistration.py
+++ python/code/GET-POST_DeviceRegistration.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 device_serial = 1
-device_mac = getMAC(wlan0)
+device_mac = getMAC('wlan0')
 print(device_mac)
 
 header = {'deviceIdentifier': device_serial, 'macAddress': device_mac}
@@ -14,7 +14,7 @@ print(reply.json())
 
 def getMAC(interface):
   try:
-    str = open('/sys/class/net/' + str(interface) + '/address').read()
+    str = open('/sys/class/net/' + interface + '/address').read()
   except:
     str = "00:00:00:00:00:00"
   return str[0:17]
