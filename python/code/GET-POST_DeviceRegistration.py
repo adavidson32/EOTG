e5d9763236933d0fe04685431f19ad9d768b64b8
adavidson32 <alex@davidson1.com>
2017-04-03 20:36:09 -0400

    Update GET-POST_DeviceRegistration.py

--- python/code/GET-POST_DeviceRegistration.py
+++ python/code/GET-POST_DeviceRegistration.py
@@ -1,6 +1,13 @@
 import requests
 import json
 
+def getMAC(interface):
+  try:
+    str = open('/sys/class/net/' + interface + '/address').read()
+  except:
+    str = "00:00:00:00:00:00"
+  return str[0:17]
+
 device_serial = 1
 device_mac = getMAC('wlan0')
 print(device_mac)
@@ -12,9 +19,4 @@ reply = requests.post("http://espressotg.com/eotg/api/devices/registerDevice", j
 print(reply.text)
 print(reply.json())
 
-def getMAC(interface):
-  try:
-    str = open('/sys/class/net/' + interface + '/address').read()
-  except:
-    str = "00:00:00:00:00:00"
-  return str[0:17]
+
