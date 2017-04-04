16c1e9b958a94c0b320cd38a7f0aebbaa311830d
adavidson32 <alex@davidson1.com>
2017-04-04 02:31:38 -0400

    Update GET-POST_getBrewSettings.py

--- python/code/GET-POST_getBrewSettings.py
+++ python/code/GET-POST_getBrewSettings.py
@@ -8,12 +8,12 @@ if len(sys.argv)==1:
   Device_ID = '1'
 else:
   Device_ID = sys.argv[1]
-print("Device_ID: ", Device_ID)
+print("Device_ID:          ", Device_ID)
 
 url_path = "/devices/getBrewSettings/"
 url_base = "http://espressotg.info/eotg/api"
 url_getBrewSettings = "%s%s%s" % (url_base, url_path, Device_ID)
-print("Connecting to: ", url_getBrewSettings, ".....")
+print("Connecting to:      ", url_getBrewSettings, ".....")
 
 response = urllib.request.urlopen(url_getBrewSettings)
 body_json = response.read().decode()
