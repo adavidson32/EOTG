b6bbca3f32f0a84d093a6cddf4748ae9eacf85fc
adavidson32 <alex@davidson1.com>
2017-04-03 22:07:53 -0400

    Update GET-POST_DeviceRegistration.py

--- python/code/GET-POST_DeviceRegistration.py
+++ python/code/GET-POST_DeviceRegistration.py
@@ -8,12 +8,25 @@ def getMAC(interface):
     str = "00:00:00:00:00:00"
   return str[0:17]
 
-device_serial = 1
+def getserial():
+  # Extract serial from cpuinfo file
+  cpuserial = "0000000000000000"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+    f.close()
+  except:
+    cpuserial = "ERROR000000000"
+  return cpuserial
+
+device_serial = getserial()
 device_mac = getMAC('wlan0')
-print(device_mac)
+print("Device Serial Number: " + device_serial)
+print("Device MAC Addr: " + device_mac)
 
 header = {'deviceIdentifier': device_serial, 'macAddress': device_mac}
-print(header)
 header_json = json.dumps(header)
 
 rep_json = requests.post("http://espressotg.info/eotg/api/devices/registerDevice", data=header_json)
@@ -21,5 +34,6 @@ rep = rep_json.json()
 rep_ID = rep['deviceId']
 print(rep)
 print(rep_ID)
+print(type(rep_ID)
 
 
