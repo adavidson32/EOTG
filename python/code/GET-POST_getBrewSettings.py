d817ff8937008407021c10fb38e1009b6be25be3
adavidson32 <alex@davidson1.com>
2017-04-04 01:43:25 -0400

    added % to url piecer function

--- python/code/GET-POST_getBrewSettings.py
+++ python/code/GET-POST_getBrewSettings.py
@@ -5,16 +5,14 @@ import json
 import sys
 
 if len(sys.argv)==1:
-  Device_ID = 1
+  Device_ID = '1'
 else:
-  print(type(sys.argv[1]))
   Device_ID = sys.argv[1]
-print("Device_ID: ", Device_ID)
-Device_ID = str(Device_ID)
+print("Device_ID: ", Device_ID , " - Device_ID Type: ", type(Device_ID))
 
 url_path = "/devices/getBrewSettings/"
 url_base = "http://espressotg.info/eotg/api"
-url_getBrewSettings = "%s%s%s" (url_base, url_path, Device_ID)
+url_getBrewSettings = "%s%s%s" % (url_base, url_path, Device_ID)
 print(url_getBrewSettings)
 
 response = urllib.request.urlopen(url_getBrewSettings)
