654a576b1a06f7289e8a9bab60765f287f7574c6
adavidson32 <alex@davidson1.com>
2017-04-03 22:46:51 -0400

    Update deviceRegistration_test.py

--- python/code/deviceRegistration_test.py
+++ python/code/deviceRegistration_test.py
@@ -27,13 +27,13 @@ device_serial = getserial()
 device_mac = getMAC('wlan0')
 print("Device Serial Number: " + device_serial)
 print("Device MAC Addr: " + device_mac)
-print("device_serial file type: " + type(device_serial))
-print("device_mac file type: " + type(device_mac))
+print("device_serial file type: " + str(type(device_serial)))
+print("device_mac file type: " + str(type(device_mac)))
 
 header = {'deviceIdentifier': device_serial, 'macAddress': device_mac}
 header_json = json.dumps(header)
-print("Header Type: " + type(header) + ". Header Text: " + header)
-print("Header_json Type: " + type(header_json) + ". Header_json Text: " + header_json)
+print("Header Type: " + str(type(header)) + ". Header Text: " + header)
+print("Header_json Type: " + str(type(header_json)) + ". Header_json Text: " + header_json)
 rep_json = requests.post("http://espressotg.info/eotg/api/devices/registerDevice", data=header_json)
 rep = rep_json.json()
 rep_ID = rep['deviceId']
