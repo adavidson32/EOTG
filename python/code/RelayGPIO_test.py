ac6eaba8d0bc1508aa80de2520c14889bb7ec354
Alex Davidson <alex@davidson1.com>
2017-04-06 22:19:08 +0400

    updated GPIO Test from Pi3

--- python/code/RelayGPIO_test.py
+++ python/code/RelayGPIO_test.py
@@ -2,13 +2,21 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(25, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(23, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(24, GPIO.OUT, initial=GPIO.HIGH)
+#NOTE: GPIO.HIGH == Relay-OFF   GPIO.LOW == Relay-OFF
+time.sleep(3)
 
-while TRUE:
-  GPIO.output(24, LOW)
-  GPIO.output(25, HIGH)
-  time.sleep(2)
-  GPIO.output(24, HIGH)
-  GPIO.output(25, LOW)
-  time.sleep(2)
+while 1:
+  GPIO.output(23, GPIO.HIGH)
+  GPIO.output(24, GPIO.LOW)
+  time.sleep(1)
+  GPIO.output(23, GPIO.LOW)
+  GPIO.output(24, GPIO.HIGH)
+  time.sleep(1)
+  GPIO.output(23, GPIO.LOW)
+  GPIO.output(24, GPIO.LOW)
+  time.sleep(1)
+  GPIO.output(23, GPIO.HIGH)
+  GPIO.output(24, GPIO.HIGH)
+  time.sleep(1)
