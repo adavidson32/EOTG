9a40dcdeccfbe1bdaf1038f7febbce59e3d54f22
adavidson32 <alex@davidson1.com>
2017-04-06 00:28:57 -0400

    Finished A0+A1 Voltage print to Serial program

--- arduino/code/Nano_A0A1_PrintSerial.py
+++ arduino/code/Nano_A0A1_PrintSerial.py
@@ -1,28 +1,21 @@
-const int pA0 = A0;  // Analog input pin that the potentiometer is attached to
-const int pA1 = A1; // Analog output pin that the LED is attached to
+// c++ program to read Arduino A0+A1 voltage, send via serial
+// Important: Serial uses 115200 speed to communicate.
+// Use A0 and A1 or change numbers in program
 
 int A0_samples = 0;        // value read from the pot
 int A1_samples = 0;
-float A0_voltage = 0;
-float A1_voltage = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
-  A0_samples = analogRead(pA0);
-  A1_samples = analogRead(pA1);
-  A0_voltage = ((A0_samples) * 5) / 1024;
-  A1_voltage = ((A1_samples) * 5) / 1024;
-  Serial.print("A0:  Voltage= ");
-  Serial.print(A0_voltage);
-  Serial.print("\t Samples= ");
-  Serial.println(A0_samples);
-  Serial.print("A1:  Voltage= ");
-  Serial.print(A1_voltage);
-  Serial.print("\t Samples= ");
-  Serial.println(A1_samples);
-  
-  delay(1);
+  A0_samples = analogRead(0);
+  A1_samples = analogRead(1);
+  Serial.print("A0: "); Serial.print(A0_samples); Serial.print(" \t A1: "); Serial.println(A1_samples);
+  float A0_voltage = ((A0_samples / 1024.00) * 5.00);
+  float A1_voltage = ((A1_samples / 1024.00) * 5.00);
+  Serial.print("A0: "); Serial.print(A0_voltage); Serial.print("V \t A1:"); Serial.print(A1_voltage); Serial.println("V");
+  Serial.println(""); Serial.println(""); Serial.println(""); Serial.println(""); Serial.println(""); 
+  delay(500);
 }
