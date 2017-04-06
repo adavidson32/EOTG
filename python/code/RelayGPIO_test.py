ff349538d28aa7656bf5b4b6be0369dcb6949be1
adavidson32 <alex@davidson1.com>
2017-04-05 23:49:43 -0400

    Update RelayGPIO_test.py

--- python/code/RelayGPIO_test.py
+++ python/code/RelayGPIO_test.py
@@ -1,14 +1,14 @@
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.OUT)
-GPIO.setup(25, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(25, GPIO.OUT, initial=GPIO.HIGH)
 
 while TRUE:
-  GPIO.output(24, FALSE)
-  GPIO.output(25, TRUE)
+  GPIO.output(24, LOW)
+  GPIO.output(25, HIGH)
   time.sleep(2)
-  GPIO.output(24, TRUE)
-  GPIO.output(25, FALSE)
+  GPIO.output(24, HIGH)
+  GPIO.output(25, LOW)
   time.sleep(2)
