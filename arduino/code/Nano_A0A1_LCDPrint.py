079dc50d2756a959353164f36ce61f3e216135cb
adavidson32 <alex@davidson1.com>
2017-04-06 00:30:08 -0400

    Create Nano_A0A1_LCDPrint.py

---
+++ arduino/code/Nano_A0A1_LCDPrint.py
@@ -0,0 +1,19 @@
+include "LCD LIB HERE"
+
+int A0_samples = 0;        // value read from the pot
+int A1_samples = 0;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  A0_samples = analogRead(0);
+  A1_samples = analogRead(1);
+  Serial.print("A0: "); Serial.print(A0_samples); Serial.print(" \t A1: "); Serial.println(A1_samples);
+  float A0_voltage = ((A0_samples / 1024.00) * 5.00);
+  float A1_voltage = ((A1_samples / 1024.00) * 5.00);
+  Serial.print("A0: "); Serial.print(A0_voltage); Serial.print("V \t A1:"); Serial.print(A1_voltage); Serial.println("V");
+  Serial.println(""); Serial.println(""); Serial.println(""); Serial.println(""); Serial.println(""); 
+  delay(500);
+}
