392301e70a963fe7b4f5967338854c77d437c119
adavidson32 <alex@davidson1.com>
2017-04-12 08:07:54 -0400

    Update Button_read.py

--- python/code/GPIO/Button_read.py
+++ python/code/GPIO/Button_read.py
@@ -2,37 +2,41 @@ import time
 import RPi.GPIO as GPIO
 
 button_pin = 21
+sampling_delay = 0.001
+debounce_time = .1
+hold_time = 2
+single_max_time = 1
+hold_timeout = 2.2
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-now = time.time()
-while not(GPIO.input(button_pin)):
-  time.sleep(.001)
-now = time.time()
-while ((GPIO.input(button_pin)) and ((time.time() - now) <= 2.2)):
-  time.sleep(.001)
-b1_time = time.time() - now
+b1_time = 0.0001
+while b1_time <= debounce_time:
+  now = time.time()
+  while not(GPIO.input(button_pin)):
+    time.sleep(sampling_delay)
+  now = time.time()
+  while ((GPIO.input(button_pin)) and ((time.time() - now) <= hold_timeout)):
+    time.sleep(sampling_delay)
+  b1_time = time.time() - now
 print("Button 1 Held for : ", b1_time, " seconds")
-if ((b1_time <= .2) and (b1_time >= .01)):
-  print("button press not long enough to register")
-  button_func = "Nothing"
-elif (b1_time >= 2):
+elif (b1_time >= hold_time):
   print("HOLD Detected")
   button_func = "HOLD"
-elif ((b1_time <= 1) and (b1_time >= .2)):
+elif ((b1_time <= single_max_time) and (b1_time >= debounce_time)):
   now = time.time()
-  while (not(GPIO.input(button_pin)) and ((time.time() - now) <= 2.2)):
-    time.sleep(.001)
+  while (not(GPIO.input(button_pin)) and ((time.time() - now) <= hold_timeout)):
+    time.sleep(sampling_delay)
   time_between = time.time() - now
   print("Time between presses : ", time_between, " seconds")
-  if time_between <= 1:
+  if time_between <= single_max_time:
     now = time.time()
-    while ((GPIO.input(button_pin)) and ((time.time() - now) <= 2.2)):
-      time.sleep(0.01)
+    while ((GPIO.input(button_pin)) and ((time.time() - now) <= hold_timeout)):
+      time.sleep(sampling_delay)
     b2_time = time.time() - now
     print("Button 2 Held for : ", b2_time, " seconds")
-    if ((b2_time >= 0.2) and (b2_time <= 1)):
+    if ((b2_time >= debounce_time) and (b2_time <= single_max_time)):
       print("2x Press Detected")
       button_func = "2x Press"
   else:
