96ef3c95fd61f05f835db2d3b716da5a3c091969
adavidson32 <alex@davidson1.com>
2017-04-12 08:13:21 -0400

    Update Button_read.py

--- python/code/GPIO/Button_read.py
+++ python/code/GPIO/Button_read.py
@@ -20,25 +20,22 @@ while b1_time <= debounce_time:
   while ((GPIO.input(button_pin)) and ((time.time() - now) <= hold_timeout)):
     time.sleep(sampling_delay)
   b1_time = time.time() - now
-print("Button 1 Held for : ", b1_time, " seconds")
 if (b1_time >= hold_time):
-  print("HOLD Detected")
   button_func = "HOLD"
 elif ((b1_time <= single_max_time) and (b1_time >= debounce_time)):
   now = time.time()
   while (not(GPIO.input(button_pin)) and ((time.time() - now) <= hold_timeout)):
     time.sleep(sampling_delay)
   time_between = time.time() - now
-  print("Time between presses : ", time_between, " seconds")
   if time_between <= single_max_time:
     now = time.time()
     while ((GPIO.input(button_pin)) and ((time.time() - now) <= hold_timeout)):
       time.sleep(sampling_delay)
     b2_time = time.time() - now
-    print("Button 2 Held for : ", b2_time, " seconds")
     if ((b2_time >= debounce_time) and (b2_time <= single_max_time)):
-      print("2x Press Detected")
       button_func = "2x Press"
   else:
-    print("1x Press Detected")
     button_func = "1x Press"
+
+print(button_func)
+return button_func
