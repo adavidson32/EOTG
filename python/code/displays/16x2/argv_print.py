8bf7583e9449fe9c0ce5a08ed48767165630ecdd
adavidson32 <alex@davidson1.com>
2017-04-12 17:46:36 -0400

    Update argv_print.py

--- python/code/displays/16x2/argv_print.py
+++ python/code/displays/16x2/argv_print.py
@@ -1,27 +1,25 @@
 import sys
 import time
+import lcddriver
+
+lcd = lcddriver.lcd()
 
 len_arg = len(sys.argv)
 if len_arg==3:
-  display_text = sys.argv[1]
-  line_num = int(sys.argv[2])
+  line1_text = sys.argv[1]
+  line2_text = sys.argv[2]
 else:
-  display_text = input("Enter text to display : ")
-  line_num = int(input("Enter line to display text on (1 or 2) : "))
-if (len(display_text) > 16):
+  print("No stings entered in argv[1] or argv[2]")
+  line1_text = input("Enter text to display on Line #1 : ")
+  line2_text = input("Enter text to display on Line #2 : ")
+if ((len(line1_text) > 16) or (len(line2_text) > 16)):
   print("ERROR: Input string over 16 characters, can't print on one line")
-  display_text = input("Enter a new string (under 16 characters) : ")
-if not(line_num==1 or line_num==2):
-  line_num = input("ERROR: invalid line number (Enter a line number as 1 or 2) : ")
+  line1_text = input("Enter text to display on Line #1 : ")
+  line2_text = input("Enter text to display on Line #2 : ")
 
 to_add = 16-len(display_text)
 for i in range(1, to_add):
   display_text += ' '
-  
-if (line_num==1):
-  print('|{}| str_len = {}'.format(display_text, len(display_text)))
-  print('|                | str_len = {}'.format((len('|                |')-2)))
-elif (line_num==2):
-  print('|                | str_len = {}'.format((len('|                |')-2)))
-  print('|{}| str_len = {}'.format(display_text, len(display_text)))
-  
+
+print('|{}|'.format(line1_text))
+print('|{}|'.format(line2_text))
