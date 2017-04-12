c51fb742e46a1dca2086adbbac39d0dc71717662
adavidson32 <alex@davidson1.com>
2017-04-12 06:12:19 -0400

    Update 16x2_print_argv.py

--- python/code/displays/16x2_print_argv.py
+++ python/code/displays/16x2_print_argv.py
@@ -1,1 +1,29 @@
+import sys
+import time
 
+len_arg = len(sys.argv)
+if len_arg==2:
+  display_text = sys.argv[1]
+  line_num = int(sys.argv[2])
+else:
+  display_text = input("Enter text to display : ")
+  line_num = input("Enter line to display text on (1 or 2) : ")
+print("Input Text: '{0}' of type {1}, Input Line: {2}".format(display_text, type(display_text), line_num))
+
+if (len(display_text) > 16):
+  print("ERROR: Input string over 16 characters, can't print on one line")
+  display_text = input("Enter a new string (under 16 characters) : ")
+if not(line_num==1 or line_num==2):
+  line_num = input("ERROR: invalid line number (Enter a line number as 1 or 2) : ")
+
+len_text = len(display_text)
+to_add = 16-len_text
+for i in range(1, to_add):
+  display_text += ' '
+  
+if (line_num==1):
+  print('|', display_text, '|')
+  print('|                |')
+else:
+  print('|                |')
+  print('|', display_text, '|')
