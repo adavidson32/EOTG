ee91ebadef32e63bcd7b4c0bfe34e8ee6b9e0332
adavidson32 <alex@davidson1.com>
2017-04-12 00:14:30 -0400

    Update main_0412.py

--- python/code/main/main_0412.py
+++ python/code/main/main_0412.py
@@ -1,3 +1,15 @@
 include time
 include sys
 include RPi.GPIO as GPIO
+
+def buttonread(read_time):
+  
+def initial_setup():
+  print("Starting program...")
+  #connect to server
+  #request updates made since last update request...
+  
+def main_loop():
+  button_result 
+  
+    
