ce7609fd873e4d19e91071542618c48a2d2a4ad9
adavidson32 <alex@davidson1.com>
2017-04-12 00:53:11 -0400

    Create strandtest_input.py

---
+++ python/code/neopixels/strandtest_input.py
@@ -0,0 +1,28 @@
+import time
+from neopixels import *
+
+LED_COUNT      = input("Number of Pixels = ")      # Number of LED pixels.
+LED_PIN        = input("Input pin (default is 18) = ")      # GPIO pin connected to the pixels (must support PWM!).
+wait_ms        = input("wait-ms (default is 20ms) = ")
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+def rainbow(strip, wait_ms, iterations=1):
+	"""Draw rainbow that fades across all pixels at once."""
+	for j in range(256*iterations):
+		for i in range(strip.numPixels()):
+			strip.setPixelColor(i, wheel((i+j) & 255))
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+    
+if __name__ == '__main__':
+	# Create NeoPixel object with appropriate configuration.
+	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+	# Intialize the library (must be called once before other functions).
+	strip.begin()
+
+	print ('Press Ctrl-C to quit.')
+	while True:
+    rainbow(srip, wait_ms)
