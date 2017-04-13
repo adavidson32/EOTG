8815a22ce4aea8ba90e4423ee12b4586dd4d5217
adavidson32 <alex@davidson1.com>
2017-04-12 23:17:53 -0400

    Update IFTTT_16x2_print.py

--- python/code/MQTT/16x2/IFTTT_16x2_print.py
+++ python/code/MQTT/16x2/IFTTT_16x2_print.py
@@ -7,6 +7,7 @@ SUB_FEED = 'ifttt'
 PUB_FEED = 'coffee_temp'
 
 sample_rate = 10.0
+coffee_temp = 10.0
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -25,11 +26,14 @@ def read_temp():
     equals_pos = lines[1].find('t=')
     temp = float(lines[1][equals_pos+2:])/1000
     return temp
-
+def clear_lcd():
+    lcd.lcd_display_string("                ", 1)
+    lcd.lcd_display_string("                ", 2)
 def connected(client):
     print("Connected to Adafruit IO!  Listening for {0} changes".format(SUB_FEED))
-    lcd.lcd_display_string("Conn:adafruit.io", 1)
-    lcd.lcd_display_string("Receiving: IFTTT", 2)
+    clear_lcd()
+    lcd.lcd_display_string("Conn adafruit.io", 1)
+    lcd.lcd_display_string("usr: {}".format(ADAFRUIT_IO_USERNAME), 2)
     client.subscribe(SUB_FEED)
 def disconnected(client):
     print('Disconnected from Adafruit IO!')
@@ -38,10 +42,17 @@ def message(client, feed_id, payload):
     print('Feed {0} received new value: {1}'.format(feed_id, payload))
     if len(payload) > 16:
         payload = payload[0:16]
-    lcd.lcd_display_string("  MSG on IFTTT  ", 1)
+    lcd_clear()
+    len_feed_name = len(feed_id)
+    extra_needed = (16 - len_feed_name - 4)
+    spacer = ''
+    for i in range(0:extra_needed):
+        spacer += ' '
+    lcd.lcd_display_string("F: {0} {1}{2:.1f}".format(feed_id, spacer, coffee_temp), 1)
     lcd.lcd_display_string("{}".format(payload), 2)
 
 lcd = lcddriver.lcd()
+clear_lcd()
 
 client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 client.on_connect    = connected
