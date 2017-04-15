78584e0eb3f6ff0549e4f2005761750b6d3f197d
adavidson32 <alex@davidson1.com>
2017-04-15 12:57:46 -0400

    Update cpu_temp_print.py

--- python/code/sensors/cpu_temp_print.py
+++ python/code/sensors/cpu_temp_print.py
@@ -4,7 +4,7 @@ def getCPUtemp():
     file = open('/sys/class/thermal/thermal_zone0/temp','r')
     lines = file.readlines()
     temp_float = float(lines[0])/1000
-    temp_str = 'CPU Temp: {.1f}*C'
+    temp_str = 'CPU Temp: {.1f}*C'.format(temp_float)
     file.close()
     return temp_float, temp_str
 
