d17927662408cbba8f58c5fefcfa090bdf312f69
adavidson32 <alex@davidson1.com>
2017-04-15 12:47:38 -0400

    Update cpu_temp_print.py

--- python/code/sensors/cpu_temp_print.py
+++ python/code/sensors/cpu_temp_print.py
@@ -3,7 +3,7 @@ import os, time
 while True:
     file = open('/sys/class/thermal/thermal_zone0/temp','r')
     lines = file.readlines()
-    temp_str = str(float(lines[0])/1000)
+    temp = float(lines[0])/1000
     file.close()
-    print("CPU Temp: {:.1f}*C".format(temp_str))
+    print("CPU Temp: {:.1f}*C".format(temp))
     time.sleep(5)
