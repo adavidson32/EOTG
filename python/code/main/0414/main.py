c2dbac4e9060ec502a4d053fe2774b4ae37e7473
Alex Davidson <alex@davidson1.com>
2017-04-15 03:12:30 -0400

    updating from desktop before going  to bed

--- python/code/main/0414/main.py
+++ python/code/main/0414/main.py
@@ -1,22 +1,10 @@
 import time, sys, math
-import RPi.GPIO as GPIO
-from mpu6050 import mpu6050
-<<<<<<< HEAD
 import os, glob
-import main_background, main_waiting, 
-=======
-import os, glob, lcddriver
-import main_background, main_waiting
->>>>>>> 0bde2bc2f2396079d6be44af3826eac19ab7473e
+import main_background, main_waiting, main_brewing
+import abc
 
 settings_all = main_settings_read()
 
-
-button_pin = settings_all[1]['pin']
-debounce_time = .1
-hold_time = 1.5
-single_max_time = 1
-hold_timeout = 1.7
 
 number_profiles = 5
 profile_num = 1
@@ -32,7 +20,7 @@ mpu6050 = mpu6050(mpu6050_addr)
 #uncomment below and change address once nano i2c slave is setup
 #LCD_16x2_addr = 0x00
 #nano_addr = 0x00
-neopixel_pin = 18    #pin must support special clock functions required for neopixels. 
+neopixel_pin = 18    #pin must support special clock functions required for neopixels.
 neopixel_ring_size = 12     #change to 24 when larger ring arrives and is used
 device_state = 'background'
 
@@ -42,5 +30,5 @@ GPIO.setup(relay_pin2, GPIO.OUT)
 GPIO.output(relay_pin1, GPIO.HIGH)
 GPIO.output(relay_pin2, GPIO.HIGH)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-  
+
 states_background()
