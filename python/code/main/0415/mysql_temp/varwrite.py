7c007c2efcc5a886d808bbb98f0d2b23eedf760b
adavidson32 <alex@davidson1.com>
2017-04-15 15:21:24 -0400

    Create varwrite.py

---
+++ python/code/main/0415/mysql_temp/varwrite.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+ex_var = {'Sensor ID': 1, 'Sample Rate': 5, 'Resolution': 0.125}
+sensor, srate, res = ex_var['Sensor ID'], ex_var['Sample Rate'], ex_var['Resolution']
+
+conn = sqlite3.connect('temp.db')
+conn.execute('''CREATE TABLE temp_settings
+                (sensor_id INTEGER, samp_rate REAL, resolution REAL, time INTEGER)''')
+conn.execute("insert into temp_settings * values (?, ?, ?, ?)", (sensor, srate, res, time.time()))
+conn.commit()
+conn.close()
+
+print("Settings successfully updated to: "
+print("Sensor #{0}: Samp_Rate({1}sec.), Resolution({2}*C".format(sensor, srate, res))
