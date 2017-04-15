1f5d718e14822cf821b6a13fb497115400c48a54
adavidson32 <alex@davidson1.com>
2017-04-15 16:51:30 -0400

    Update varread.py

--- python/code/main/0415/mysql_temp/varread.py
+++ python/code/main/0415/mysql_temp/varread.py
@@ -1,9 +1,11 @@
-import sqlite3
-import time, os
+import sqlite3, time
 
 def get_variables():
-    con = sqlite3.connect('eotg.db')
-    c = con.cursor()
-    c.execute('select * from temp_vals')
-    r = c.fetchone()
-    
+    conn = sqlite3.connect('temp.db')
+    c = conn.cursor
+    c.execute('SELECT * FROM temp_settings')
+    select_return = c.fetchone()
+    conn.close()
+    temp_settings_dict = {'Sensor ID:' select_return[0], 'Sampling Rate': select_return[1], 'Resolution': select_return[2]}
+    update_time = time.time()
+    return temp_settings_dict, update_time
