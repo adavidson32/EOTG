c5aa25aebb6a33e0c0d87ff4b13dae9c5963fb4d
adavidson32 <alex@davidson1.com>
2017-04-16 00:03:56 -0400

    Update cpu_temp-use_print.py

--- python/code/16x2/cpu_temp-use_print.py
+++ python/code/16x2/cpu_temp-use_print.py
@@ -21,6 +21,8 @@ while True:
     use_p = getCPUuse()
     print("CPU Use: ", use_p, " %") 
     print("CPU Temp: ", temp_str)
-    lcd.lcd_display_string('Use: %s % ' % use_p, 1)
-    lcd.lcd_display_string('Temp: %s *C ' % temp_str, 2)
+    str1 = 'CPU Use: ' + use_p + '%  '
+    str2 = 'CPU Temp: ' + temp_str + '*C '
+    lcd.lcd_display_string(str1, 1)
+    lcd.lcd_display_string(str2, 2)
     time.sleep(1)
