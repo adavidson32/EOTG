82b983caface8c66310d83846bc56402045d9abf
adavidson32 <alex@davidson1.com>
2017-04-15 23:05:07 -0400

    Update test_temp_print.py

--- python/code/sqlite3/test_temp_print.py
+++ python/code/sqlite3/test_temp_print.py
@@ -1,4 +1,5 @@
-import sqlite3, time, os, glob, sys
+import sqlite3
+import time
 from ds18b20 import DS18B20
 
 x = DS18B20()
@@ -6,14 +7,19 @@ x = DS18B20()
 upload_time = time.time()
 
 while True:
+    
     while (time.time() < (upload_time + 5)):
         time.sleep(.1)
+        
     upload_time = time.time()
     dt = time.strftime('%x')
     tim = time.strftime('%X')
+    
     conn = sqlite3.connect('temp.db')
     c = conn.cursor()
-    c.execute("INSERT INTO temp_values VALUES (1,?,?,?)".format(x.tempC(0), dt, tim))
+    value_tuple = (1, float(x.tempC(0), 1), dt, tim)
+    c.execute('INSERT INTO temp_values VALUES (?,?,?,?)', value_tuple)
     conn.commit()
     conn.close()
+    
     print("Just added T={0:.1f}*C to temp.db".format(x.temp(0)))
