33cf12043a2b2ee7453959bd18d69d45041e5166
adavidson32 <alex@davidson1.com>
2017-04-15 22:24:51 -0400

    Update db_setup.py

--- python/code/sqlite3/db_setup.py
+++ python/code/sqlite3/db_setup.py
@@ -4,7 +4,9 @@ conn = sqlite3.connect('temp.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE temp_values
              (sensor_id INTEGER, temp_c REAL, date_str TEXT, time_str TEXT)''')
-c.exectute("INSERT INTO temp_values VALUES (0, 1.00, time.strftime("%x"), time.strftime("%X"))")
+date_s = time.strftime("%x")
+time_s = time.strftime("%X")
+c.exectute("INSERT INTO temp_values VALUES (0, 1.00, date_s, time_s)")
 # insert initial "fake" values for some senser #0: Temp = 1.00*C, time = now....
 conn.commit()
 conn.close()
