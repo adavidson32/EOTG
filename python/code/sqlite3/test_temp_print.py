023c74530b32aa52fbf0be58fbd99a57174adeb1
adavidson32 <alex@davidson1.com>
2017-04-15 22:17:31 -0400

    Update test_temp_print.py

--- python/code/sqlite3/test_temp_print.py
+++ python/code/sqlite3/test_temp_print.py
@@ -1,30 +1,7 @@
 import sqlite3, time, os, glob, sys
 import ds18b20 as DS18B20
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
-
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
-
-def read_temp_raw():
-  f = open(device_file, 'r')
-  lines = f.readlines()
-  f.close()
-  return lines
-
-def read_temp():
-  lines = read_temp_raw()
-  while lines[0].strip()[-3:] != 'YES':
-    time.sleep(0.2)
-    lines = read_temp_raw()
-  equals_pos = lines[1].find('t=')
-  if equals_pos != -1:
-    temp_string = lines[1][equals_pos+2:]
-    temp_c = float(temp_string) / 1000.0
-    temp_f = temp_c * 9.0 / 5.0 + 32.0
-    return temp_c
+x = DS18B20()
 
 upload_time = time.time()
 
@@ -36,6 +13,6 @@ While True:
     time_str = time.strftime('%X')
     conn = sqlite3.connect('temp.db')
     c = conn.cursor()
-    c.execute("INSERT INTO temp_values VALUES (1, read_temp(), date_str, time_str)")
+    c.execute("INSERT INTO temp_values VALUES (1, x.tempC(0), date_str, time_str)")
     conn.commit()
     conn.close()
