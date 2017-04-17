79c171d18243ddee216af64ab837081719e874e3
Alex Davidson <alex@davidson1.com>
2017-04-17 15:39:13 -0400

    from desktop

--- python/code/main/0417/setup.py
+++ python/code/main/0417/setup.py
@@ -8,8 +8,6 @@ def var_setup():
     conn = sqlite3.connect('eotg.db')
     c = conn.cursor()
     button_var_list = neopixel_var_list = wifi_var_list = []
-    neopixel_var_list = []
-    wifi_var_list = []
     for row in c.execute('SELECT * FROM setup_variables WHERE category=?', ('button',)):
         var_tuple = (row[1], row[2])
         button_var_list.append(var_tuple)
@@ -19,6 +17,9 @@ def var_setup():
     for row in c.execute('SELECT * FROM wifi_variables'):
         var_list = [('priority', row[0]), ('ssid', row[1]), ('password', row[2])]
         wifi_var_list.append(var_list)
+    print(button_var_list)
+    print(neopixel_var_list)
+    print(wifi_var_list)
     button_var = dict(button_var_list)
     neopixel_var = dict(neopixel_var_list)
     wifi_var = dict(wifi_var_list)
