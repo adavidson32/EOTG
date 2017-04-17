1f47ef8fab5d89c9f656f84f86c3681a5dccd19a
Alex Davidson <alex@davidson1.com>
2017-04-17 15:20:01 -0400

    from desktop

--- python/code/main/0417/setup.py
+++ python/code/main/0417/setup.py
@@ -1,25 +1,43 @@
 import os, glob, time, math
-import sqlite3, lcddriver
-from ds18b20 import DS18B20
-from mpu6050 import mpu6050
+import sqlite3
+#from ds18b20 import DS18B20
+#from mpu6050 import mpu6050
 
 def var_setup():
     var_list = (('button',), ('neopixel',), ('wifi',), ('ds18b20',), ('bmp280',), ('mpu6050',))
     conn = sqlite3.connect('eotg.db')
     c = conn.cursor()
-    for a in var_list:
-        for row in c.execute('SELECT * FROM setup_variables WHERE category=?', var_list[a]):
-
-
+    button_var_list = neopixel_var_list = wifi_var_list = []
+    neopixel_var_list = []
+    wifi_var_list = []
+    for row in c.execute('SELECT * FROM setup_variables WHERE category=?', ('button',)):
+        var_tuple = (row[1], row[2])
+        button_var_list.append(var_tuple)
+    for row in c.execute('SELECT * FROM setup_variables WHERE category=?', ('neopixel',)):
+        var_tuple = (row[1], row[2])
+        neopixel_var_list.append(var_tuple)
+    for row in c.execute('SELECT * FROM wifi_variables'):
+        wifi_var_list.append(row)
+    button_var = dict(button_var_list)
+    neopixel_var = dict(neopixel_var_list)
+    wifi_var = dict(wifi_var_list)
     #connect to sqlite database (ex. eotg.db)
     #retrieve all values from setting_variables table in eotg.db
     #return tuple of all variable dictionairies ex: ({'t_hold': 1.5, ...}, {'neopixel_pin': 18, ...})
-    output_tuple = (button_var, neopixel_var, wifi_var, ds18b20_var, mpu6050_var, bmp280_var, relay_var)
+    output_tuple = (button_var, neopixel_var, wifi_var)
     return output_tuple
 
-def hw_setup(hw_addresses):
+#def hw_setup(hw_addresses):
     #import: RPi-GPIO, bmp280, ds18b20, mpu6050, 16x2 lcd libraries
     #setup relay GPIO in correct modes
     #setup BMP280, MPU6050 I2C devices (based on saved I2C addresses from variable_setup)
     #setup DS18B20 sensors (identify all, save some variables?)
-    return num_ds18b20,
+    #return num_ds18b20,
+
+button_var, neopixel_var, wifi_var = var_setup()
+print('Button Variables: ')
+print(button_var)
+print('Neopixel Variables: ')
+print(neopixel_var)
+print('Wifi Variables: ')
+print(wifi_var)
