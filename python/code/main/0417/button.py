182929b1139dbcba26ed6c3669aa6603c25fbded
Alex Davidson <alex@davidson1.com>
2017-04-17 21:25:44 -0400

    from desktop

--- python/code/main/0417/button.py
+++ python/code/main/0417/button.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-import time, sqlite3
+import time, sqlite3, math
 
 def settings_read():
     conn = sqlite3.connect('eotg.db')
@@ -9,6 +9,16 @@ def settings_read():
         button_tuple = (row[0], row[1])
         button_settings_list.append(button_tuple)
     return dict(button_settings_list)
+
+def store_press(press_type):
+    conn = sqlite3.connect()
+    c = conn.cursor()
+    time_epoch = (math.floor(time.time()),)
+    press_type_t = (press_type,)
+    c.execute('INSERT INTO button_events VALUES (?, ?)' (press_type_t, time_epoch))
+    conn.commit()
+    conn.close()
+    print('Added to eotg.db:   Detected {} @ {}'.format(press_type, time_epoch))
 
 def button_interupt_handler(button_pin):
     GPIO.remove_event_detect(button_pin)
@@ -20,23 +30,23 @@ def button_interupt_handler(button_pin):
         print('hold detected')
         GPIO.remove_event_detect(button_pin)
         GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
-        return 'hold'
+        store_press('hold')
     else:
-        if (t_1x > 1.5):
+        if (t_1x > 2):
             print('hold detected')
             GPIO.remove_event_detect(button_pin)
             GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
-            return 'hold'
+            store_press('hold')
         elif (t_1x < 0.1):
             print('too quick')
             GPIO.remove_event_detect(button_pin)
             GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
-            return 'nothing'
+            return
         elif (t_1x > 1):
-            print('too long')
+            print('too long but not hold (1s < t < 2s)')
             GPIO.remove_event_detect(button_pin)
             GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
-            return 'nothing'
+            return
         else:
             GPIO.remove_event_detect(button_pin)
             ret1 = GPIO.wait_for_edge(button_pin, GPIO.RISING, timeout=1000)
@@ -44,7 +54,7 @@ def button_interupt_handler(button_pin):
                 print('1x press detected')
                 GPIO.remove_event_detect(button_pin)
                 GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
-                return '1x_press'
+                store_press('1x')
             elif ((time.time() - t_1x_end) > 0.1):
                 GPIO.remove_event_detect(button_pin)
                 ret2 = GPIO.wait_for_edge(button_pin, GPIO.FALLING, timeout=1000)
@@ -52,19 +62,18 @@ def button_interupt_handler(button_pin):
                     print('2nd press too long')
                     GPIO.remove_event_detect(button_pin)
                     GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
-                    return 'nothing'
+                    return
                 else:
                     print('2x press detected')
                     GPIO.remove_event_detect(button_pin)
                     GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
-                    return '2x_press'
+                    store_press('2x')
 
 bsettings = settings_read()
 button_pin = int(bsettings['pin'])
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-a = GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
-i = 1
+GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
+
 while True:
-    time.sleep(5)
-    print(a)
+    time.sleep(1)
