27d2099309d7ce78a596084a2c398fb649311af1
Alex Davidson <alex@davidson1.com>
2017-04-17 20:52:50 -0400

    from desktop

--- python/code/main/0417/button.py
+++ python/code/main/0417/button.py
@@ -11,25 +11,35 @@ def settings_read():
     return dict(button_settings_list)
 
 def button_interupt_handler(button_pin):
-    print('button_press detected, printing button setting info...')
-    bsettings = settings_read()
-    button_pin = bsettings['pin']
-    print('button settings dictionairy: ')
-    print(bsettings)
-    print('button pin value:')
-    print(button_pin)
-    print("type of 'button_pin': ")
-    print(type(button_pin))
+    t_1x_start = time.time()
+    ret = GPIO.wait_for_edge(button_pin, GPIO.FALLING, timeout=5000)
+    t_1x_end = time.time()
+    t_1x = t_1x_end - t_1x_start
+    if ret is None:
+        return 'too_long'
+    else:
+        if (t_1x > 2):
+            return 'hold'
+        elif (t_1x < 0.1):
+            return 'nothing'
+        elif (t_1x > 1):
+            return 'nothing'
+        else:
+            ret = GPIO.wait_for_edge(bottom_pin, GPIO.RISING, timeout=1000)
+            if ret is None:
+                return '1x_press'
+            else:
+                ret = GPIO.wait_for_edge(bottom_pin, GPIO.FALLING, timeout=1000)
+                if ret is None:
+                    return 'nothing'
+                else:
+                    return '2x_press'
 
 bsettings = settings_read()
 button_pin = int(bsettings['pin'])
-print(button_pin)
-print(type(button_pin))
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
 i = 1
 while True:
     time.sleep(1)
-    print('i = {}'.format(i))
-    i += 1
