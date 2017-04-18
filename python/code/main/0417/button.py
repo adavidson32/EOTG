edc875dde6219dfe330e3b3e95e40c45236f4add
Alex Davidson <alex@davidson1.com>
2017-04-17 21:14:55 -0400

    from desktop

--- python/code/main/0417/button.py
+++ python/code/main/0417/button.py
@@ -13,14 +13,14 @@ def settings_read():
 def button_interupt_handler(button_pin):
     GPIO.remove_event_detect(button_pin)
     t_1x_start = time.time()
-    ret = GPIO.wait_for_edge(button_pin, GPIO.FALLING, timeout=5000)
+    ret = GPIO.wait_for_edge(button_pin, GPIO.FALLING, timeout=3000)
     t_1x_end = time.time()
     t_1x = t_1x_end - t_1x_start
     if ret is None:
-        print('way too long, >5s')
+        print('hold detected')
         GPIO.remove_event_detect(button_pin)
         GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
-        return 'too_long'
+        return 'hold'
     else:
         if (t_1x > 1.5):
             print('hold detected')
@@ -63,7 +63,8 @@ bsettings = settings_read()
 button_pin = int(bsettings['pin'])
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
+a = GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_interupt_handler)
 i = 1
 while True:
-    time.sleep(1)
+    time.sleep(5)
+    print(a)
