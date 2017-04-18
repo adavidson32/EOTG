6b58639d4d19ed41f8bf264838d74c421fcc3312
Alex Davidson <alex@davidson1.com>
2017-04-18 00:49:20 -0400

    from desktop

--- python/code/main/0417/main.py
+++ python/code/main/0417/main.py
@@ -3,14 +3,19 @@ from setup import var_setup, sensor_setup
 
 def read_settings():
     ret_var = var_setup()
+    device_settings, button_settings, button_events, wifi_settings = ret_var
     print('')
     print('Variable Setup Return: ')
-    print(ret_var)
+    print(device_settings)
+    print(button_settings)
+    print(button_events)
+    print(wifi_settings)
 
     ret_sensor = sensor_setup()
     print('')
     print('Sensor Return')
-    print(ret_sensor)
+    print(ds.getC(0))
+    print(mpu.get_accel_data())
 
 read_settings()
 print('done with simple main.py....')
