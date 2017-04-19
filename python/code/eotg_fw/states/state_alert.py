613b0f3f341d12f35561e2b12d3ca74d4ca494d6
Alex Davidson <Alex Davidson>
2017-04-19 18:52:48 -0400

    from surface

--- python/code/eotg_fw/states/state_alert.py
+++ python/code/eotg_fw/states/state_alert.py
@@ -3,6 +3,6 @@ import sqlite3
 def sqlite_update(table, setting, new_value):
     conn = sqlite3.connect('../main/eotg.db')
     c = conn.cursor()
-    c.execute('UPDATE ? SET ?=?',(table, setting, new_value))
+    c.execute('UPDATE (?) SET (?)=(?)',(table, setting, new_value))
     conn.commit()
     conn.close()
