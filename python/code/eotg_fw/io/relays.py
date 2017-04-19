969d43fee58d30d6c33629ab997824256422fa79
Alex Davidson <Alex Davidson>
2017-04-19 18:47:29 -0400

    from surface

--- python/code/eotg_fw/io/relays.py
+++ python/code/eotg_fw/io/relays.py
@@ -2,20 +2,11 @@ import RPi.GPIO as io
 from time import sleep, localtime, strftime, time
 
 class relays:
-    def __init__(self, pump_pin, heat_pin):
+    def __init__(self, pin):
         io.setmode(GPIO.BCM)
-        io.setup(pump_pin, GPIO.OUT, initial=GPIO.HIGH)
-        io.setup(heat_pin, GPIO.OUT, initial=GPIO.HIGH)
+        io.setup(pin, io.OUT, initial=io.HIGH)
 
-    def pump_on(self):
-        io.output(pump_pin, GPIO.LOW)
-    def pump_off(self):
-        io.output(pump_pin, GPIO.HIGH)
-    def heat_on(self):
-        io.output(heat_pin, GPIO.LOW)
-    def heat_off(self):
-        io.output(heat_pin, GPIO.HIGH)
-    def pump_on_for(self, sec_on):
-        now = time()
-    def heat_on_for(self, sec_on):
-        now = time()
+    def on(self):
+        io.output(pin, io.LOW)
+    def off(self):
+        io.output(pin, io.HIGH)
