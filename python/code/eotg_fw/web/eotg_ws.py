67a2b32f5cb5fa62d62c1fccac3549c38cbfe71d
Alex Davidson <Alex Davidson>
2017-04-19 22:32:08 -0400

    from surface

--- python/code/eotg_fw/web/eotg_ws.py
+++ python/code/eotg_fw/web/eotg_ws.py
@@ -74,7 +74,7 @@ def shouldBrew():
 def brewStarted():
     try:
         # Tell the server we have started brewing and don't need to have brew state high anymore.
-        resp = httpRequest.makeRequest(ws.getWs('setBrewEnable'), None, ['0'])
+        resp = httpRequest.makeRequest(ws.getWs('setBrewEnable'), ('empty', 'crap'), ['0'])
         responseErr = json.loads(resp)['error']
         responseMsg = json.loads(resp)['message']
         if responseErr:
