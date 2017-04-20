22e5dd6b1c9a3e8bce00d5a23fcfa04e7fa4b734
Alex Davidson <Alex Davidson>
2017-04-19 21:48:07 -0400

    from surface

--- python/code/eotg_fw/states/waiting.py
+++ python/code/eotg_fw/states/waiting.py
@@ -14,6 +14,7 @@ def waiting(all_settings):
     if loop_exit == 'hold_detected':
         return 'background'
     elif ((loop_exit == '2x_detected') or (loop_exit == 'remote_start_detected')):
+        #add to-do to re-set remote_start_detected to 0
         return 'brewing'
 
 def waiting_loop(all_settings, t_last_button_check):
@@ -27,17 +28,17 @@ def waiting_loop(all_settings, t_last_button_check):
     detect_t = ('TOTAL CRAP', )
     if last_press is None:
          time.sleep(1)
-         return waiting_loop(all_settings, t_last_button_check)
+         # If no buttons are pressed, get the status from the web
+         shouldBrew = shouldBrew()
+         if shouldBrew == 1:
+             return ('remote_start_detected', 10.0)
+         else:
+             return waiting_loop(all_settings, t_last_button_check)
     if last_press[0] == 'hold':
         detect_t = ('hold_detected', t_last_button_check)
     elif last_press[0] == '1x':
         detect_t = ('1x_detected', t_last_button_check)
     elif last_press[0] == '2x':
         detect_t = ('2x_detected', t_last_button_check)
-    else:
-        # If no buttons are pressed, get the status from the web
-        shouldBrew = shouldBrew()
-        if shouldBrew == 1:
-            return 'remote_start_detected'
     if not(last_press is None):
         return detect_t
