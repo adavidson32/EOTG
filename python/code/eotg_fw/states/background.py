d5e6dcf4616ac3de6778c212c5a4909249a93d5d
Alex Davidson <Alex Davidson>
2017-04-21 17:51:18 -0400

    from surface

--- python/code/eotg_fw/states/background.py
+++ python/code/eotg_fw/states/background.py
@@ -4,7 +4,7 @@ import sqlite3
 
 def background(all_settings):
     print('New State: Background')
-    t_last_button_check = time.time()-0.3
+    t_last_button_check = time.time()-0.5
     sqlite_update('device_info', 'current_state', 'background')
     background_loop_return = background_loop(all_settings, t_last_button_check)
     loop_exit, t_last_button_check = background_loop_return
--- python/code/eotg_fw/states/waiting.py
+++ python/code/eotg_fw/states/waiting.py
@@ -37,8 +37,8 @@ def waiting_loop(all_settings, t_last_button_check):
     if last_press[0] == 'hold':
         detect_t = ('hold_detected', t_last_button_check)
     elif last_press[0] == '1x':
-        print('next state selected...')
-        print('switching device to next state.....')
+        print('next profile selected...')
+        print('switching device to next profile.....')
         detect_t = ('1x_detected', t_last_button_check)
     elif last_press[0] == '2x':
         detect_t = ('2x_detected', t_last_button_check)
