4bf18a7920918c6aea4d735be1920e4ae93a8099
Alex Davidson <Alex Davidson>
2017-04-21 18:05:16 -0400

    from surface

--- python/code/eotg_fw/main/main.py
+++ python/code/eotg_fw/main/main.py
@@ -8,6 +8,7 @@ from setup import variable_setup, sensor_setup
 from background import background
 from waiting import waiting
 from brewing import brewing
+from check_orientation import check_orientation
 
 all_settings = variable_setup()
 
@@ -22,14 +23,22 @@ while True:
         if background_return == 'waiting':
             current_state = 'waiting'
         elif background_return == 'brewing':
-            current_state = 'brewing'
+            mpu_test = check_orientation(mpu)
+            if mpu_test == 'level':
+                current_state = 'brewing'
+            else:
+                current_state = 'background'
     elif current_state == 'waiting':
         waiting_return = waiting(all_settings)
         time.sleep(1)
         if waiting_return == 'background':
             current_state = 'background'
         elif waiting_return == 'brewing':
-            current_state = 'brewing'
+            mpu_test = check_orientation(mpu)
+            if mpu_test == 'level':
+                current_state = 'brewing'
+            else:
+                current_state = 'waiting'
     elif current_state == 'brewing':
         brewing_return = brewing(all_settings, sensors)
         time.sleep(1)
