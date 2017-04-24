9dab5f295f919613de8065682fba3c98d1275a57
Alex Davidson <Alex Davidson>
2017-04-23 21:38:47 -0400

    from surface

--- python/code/eotg_fw/states/state_alert.py
+++ python/code/eotg_fw/states/state_alert.py
@@ -6,9 +6,9 @@ def sqlite_update(table, setting, new_value):
     conn = sqlite3.connect('../main/eotg.db')
     c = conn.cursor()
     sqlCommand = ("UPDATE {0} SET {1} = '{2}'".format(table, setting, new_value))
-    print(sqlCommand)
+'''    print(sqlCommand)
     print(type(sqlCommand))
-    print(len(sqlCommand))
+    print(len(sqlCommand)) '''
     c.execute(sqlCommand)
     conn.commit()
     conn.close()
