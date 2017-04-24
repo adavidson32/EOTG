738694d3d12e28ebdde5c8b21071c5ceed157c80
Alex Davidson <Alex Davidson>
2017-04-23 22:13:09 -0400

    from surface

--- python/code/eotg_fw/states/brewing.py
+++ python/code/eotg_fw/states/brewing.py
@@ -20,7 +20,6 @@ def brewing(all_settings, sensors):
     if (loop_exit == '2x_detected'):
         print('2x detected, cancelling brew, returning to waiting state')
         pump.pwm_stop()
-        pump.off()
         heater.off()
         return 'waiting'
     elif (loop_exit == 'hold_detected'):
@@ -31,7 +30,6 @@ def brewing(all_settings, sensors):
         #pump.off(all_settings['pump_settings']['pin'])
         #heater.off(all_settings['heater_settings']['pin'])
         pump.pwm_stop()
-        pump.off()
         heater.off()
         return 'waiting'
 
