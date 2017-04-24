78723dfb01295f84c969eb62b83be4f92ccf17a3
Alex Davidson <Alex Davidson>
2017-04-23 23:42:18 -0400

    from surface, change to 9s off, 1s on

--- python/code/eotg_fw/states/brewing.py
+++ python/code/eotg_fw/states/brewing.py
@@ -12,6 +12,15 @@ def brewing(all_settings, sensors):
     pump.pwm_start()
     heater.on()
     t_brew_start = time.time()
+    conn = sqlite3.connect('../main/eotg.db')
+    c = conn.cursor()
+    while time.time() < (t_brew_start + 21.0):
+        c.execute("SELECT * FROM button_events WHERE detect_time>?", (t_brew_start,))
+        last_press = c.fetchone()
+        if last_press[0] == '2x':
+            return 'waiting'
+    conn.commit()
+    conn.close()
     t_brew_end = t_brew_start + 60.0
     t_last_button_check = time.time()-0.3
     brewing_loop_return = brewing_loop(all_settings, t_last_button_check, t_brew_end)
