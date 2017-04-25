fbb6b8e35d8c6d87559db5db1e3a4e0b91bc1f33
Alex Davidson <Alex Davidson>
2017-04-25 11:33:53 -0400

    from surface

--- python/code/eotg_fw/sensors/prebrew_check.py
+++ python/code/eotg_fw/sensors/prebrew_check.py
@@ -1,7 +1,7 @@
 import math, sqlite3
 import RPi.GPIO as io
 from state_alert import sqlite_update
-from eotg_ws import putDeviceStatus
+from eotg_ws import eotg_ws
 
 class pb_check:
     def __init__(self, ac_batt_settings):
@@ -19,7 +19,7 @@ class pb_check:
         print('ac_state: {}'.format(ac_state))
         if not(ac_state == ac_last):
             sqlite_update('device_info', 'ac_state', ac_state)
-            putDeviceStatus()
+            eotg_ws.putDeviceStatus()
         return ac_ret
 
     def battery_check(self, batt_last):
@@ -28,7 +28,7 @@ class pb_check:
         print('batt_level: {}'.format(batt_level))
         if not(battery_level == batt_last):
             sqlite_update('device_info', 'battery_level', battery_level)
-            putDeviceStatus()
+            eotg_ws.putDeviceStatus()
         return battery_level
 
     def check_orientation(self, mpu):
