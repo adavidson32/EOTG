31f6e85b28d13ec6cab4796697265d6056409d7c
Alex Davidson <Alex Davidson>
2017-04-24 23:32:44 -0400

    from surface

--- python/code/eotg_fw/web/brewUpdateWorker.py
+++ python/code/eotg_fw/web/brewUpdateWorker.py
@@ -12,18 +12,17 @@ class BrewUpdateWorker(httpUpdateWorker.HttpUpdateWorker):
         # Run the brew monitor
         while(self.stopped != True):
             try:
-               # Get the brew status from the web server and set the brew status in the database
-               print('Checking if we should brew.')
-               shouldBrew()
-               # Get how long we should sleep for, then sleep for that long.
-               brewCheckPeriod = 3 #super().getTiming()
-               if brewCheckPeriod > 0:
-                   time.sleep(brewCheckPeriod)
-               else:
-                   # Default to 10 seconds
-                   time.sleep(10)
+                # Get the brew status from the web server and set the brew status in the database
+                print('Checking if we should brew.')
+                shouldBrew()
+                # Get how long we should sleep for, then sleep for that long.
+                brewCheckPeriod = 3 #super().getTiming()
+                if brewCheckPeriod > 0:
+                    time.sleep(brewCheckPeriod)
+                else:
+                    # Default to 10 seconds
+                    time.sleep(10)
             except Exception as ex:
                 print('Error getting brew enable: ' + str(ex))
-    			self.stop()
-
+                self.stop()
         self.stop()
