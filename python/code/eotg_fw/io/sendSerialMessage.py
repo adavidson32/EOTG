16e02ba77211222d2181ff64dfbb4aceb55a48b3
Alex Davidson <alex@davidson1.com>
2017-04-25 08:00:13 -0400

    from desktop

--- python/code/eotg_fw/io/sendSerialMessage.py
+++ python/code/eotg_fw/io/sendSerialMessage.py
@@ -3,12 +3,12 @@ import serial
 def sendSerialMsg(msg, serial_port, baud_rate, to):
 
     ser = serial.Serial(
-        port = serial_port,
-        baudrate = baud_rate,
+        port = '/dev/ttyAMA0',
+        baudrate = 9600,
         parity = serial.PARITY_NONE,
         stopbits = serial.STOPBITS_ONE,
         bytesize = serial.EIGHTBITS,
-        timeout = to
+        timeout = 1
     )
     print('writing ' + msg + ' to port ' + str(serial_port))
     ser.write(msg)
