5337b9c42b660bce63aef5049a9666b249dbe414
Alex Davidson <Alex Davidson>
2017-04-25 04:58:18 -0400

    from surface: added setup stuff to SerialNeoPixelControl.ino

--- arduino/code/SerialNeoPixelControl.ino
+++ arduino/code/SerialNeoPixelControl.ino
@@ -1,1 +1,28 @@
+#include <NeoPixelBus.h>
 
+const uint16_t PixelCount = 24; // this example assumes 4 pixels, making it smaller will cause a failure
+const uint8_t PixelPin = 11;  // make sure to set this to the correct pin, ignored for Esp8266
+
+#define colorSaturation 128
+
+NeoPixelBus<NeoRgbwFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
+
+RgbColor red(colorSaturation, 0, 0);
+RgbColor green(0, colorSaturation, 0);
+RgbColor blue(0, 0, colorSaturation);
+RgbColor white(colorSaturation);
+RgbColor black(0);
+
+void setup()
+{
+    Serial.begin(115200);
+    while (!Serial); // wait for serial attach
+    Serial.println(); Serial.println("Initializing..."); Serial.flush();
+    strip.Begin(); strip.Show();
+    Serial.println();  Serial.println("Running...");
+}
+
+void loop()
+{
+  
+}
