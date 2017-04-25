1ed744dea3338ae9592efc53c168d8236d3a10af
Alex Davidson <Alex Davidson>
2017-04-25 11:46:34 -0400

    from surface

--- python/code/eotg_fw/setup/setup.py
+++ python/code/eotg_fw/setup/setup.py
@@ -63,7 +63,7 @@ def variable_setup():
 
     suw = statusUpdateWorker.StatusUpdateWorker()
     buw = brewUpdateWorker.BrewUpdateWorker()
-    but = button.button()
+    but = button()
     t_suw = Thread(target=suw.runStatusMonitor(), args=())
     t_buw = Thread(target=buw.runBrewMonitor(), args=())
     t_but = Thread(target=but.button_manager(), args=())
